54014d0468d368f73142bb41e2bfb7fda6eadcfb
mattes <matthias.kadenbach@gmail.com>
2013-04-11 19:24:34 +0200

    working rover start position

--- classes/Rover.js
+++ classes/Rover.js
@@ -1,13 +1,22 @@
 "use strict";
+var _ = require('underscore');
 var Position = require('./Position.js');
 
-function Rover() {
-  this.pathStack = []; 
+function Rover(options) {
+  var defaults = {
+    start: new Position(0, 0, 'north') // default position
+  };
+
+  if(!options) options = {};
+  options = _.defaults(options, defaults);
+
+  this.pathStack = [];
+  this.pathStack.push(options.start); 
 }
 
 Rover.prototype.getPosition = function() {
   if(this.pathStack.length === 0) return false;
-  return this.pathStack[this.pathStack.length-1].Position;
+  return this.pathStack[this.pathStack.length-1];
 }
 
 Rover.prototype.move = function() {
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -5,12 +5,20 @@ var Position = require('../classes/Position.js');
 
 describe('Rover', function(){
 
-  var rover = new Rover({start: new Position(0, 0, 'north')});
+  it('should start at position 0,0 facing north (per default)', function(){
+    var rover = new Rover();
+    var startPosition = rover.getPosition();
+    expect(startPosition.x).toEqual(0);
+    expect(startPosition.y).toEqual(0);
+    expect(startPosition.direction).toEqual('north');
+  });
 
-  it('should start at position 0,0 facing north', function(){
-    expect(rover.getPosition().x).toEqual(0);
-    expect(rover.getPosition().y).toEqual(0);
-    expect(rover.getPosition().direction).toEqual('north');
+  it('should start at position 1,2 facing south (per argument)', function(){
+    var rover = new Rover({start: new Position(1, 2, 'south')});
+    var startPosition = rover.getPosition();
+    expect(startPosition.x).toEqual(1);
+    expect(startPosition.y).toEqual(2);
+    expect(startPosition.direction).toEqual('south');
   });
 
 })
