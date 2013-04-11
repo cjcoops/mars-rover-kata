2c00187e39fa5a536f4dfe13c544955b6268b74c
mattes <matthias.kadenbach@gmail.com>
2013-04-11 19:43:38 +0200

    implementing moveForward and moveBackward

--- classes/Rover.js
+++ classes/Rover.js
@@ -11,7 +11,7 @@ function Rover(options) {
   options = _.defaults(options, defaults);
 
   this.pathStack = [];
-  this.pathStack.push(options.start); 
+  this.addPosition(options.start); 
 }
 
 // get last Position of rover
@@ -21,9 +21,39 @@ Rover.prototype.getPosition = function(index) {
   return this.pathStack[this.pathStack.length - index - 1];
 }
 
+// add new Position to rover path
+Rover.prototype.addPosition = function(position) {
+  this.pathStack.push(position);
+  return true;
+}
+
 // move rover
 Rover.prototype.move = function(commandString) {
-  return true;
+  if(!_.isString(commandString)) throw new Error('invalid command string');
+  var commands = commandString.split('');
+  if(commands.length === 0) return true; // everything is fine
+
+  // iterate over every single command
+  for(var i=0; i < commands.length; i++) {
+    switch(commands[i]) {
+      case 'f': this.moveForward(); break;
+      case 'b': this.moveBackward(); break;
+    }
+  }
+}
+
+// move rover forward and save new position
+Rover.prototype.moveForward = function() {
+  var position = this.getPosition();
+  position.y++;
+  this.addPosition(position); 
+}
+
+// move rover backward and save new position
+Rover.prototype.moveBackward = function() {
+  var position = this.getPosition();
+  position.y--;
+  this.addPosition(position); 
 }
 
 module.exports = Rover;
