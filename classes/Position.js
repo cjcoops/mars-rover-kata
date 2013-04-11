1ce9dfc3b7608631c6e3b0743172b9cae107b1ab
mattes <matthias.kadenbach@gmail.com>
2013-04-11 18:59:18 +0200

    underscore lib added

--- classes/Position.js
+++ classes/Position.js
@@ -1,10 +1,11 @@
 "use strict";
+var _ = require('underscore');
 
 var DIRECTIONS = ['north', 'east', 'south', 'west'];
 
 function Position(x, y, direction) {
-  if(isNaN(x)) throw new Error('missing x coordinate');
-  if(isNaN(y)) throw new Error('missing y coordinate');
+  if(!_.isNumber(x)) throw new Error('missing x coordinate');
+  if(!_.isNumber(y)) throw new Error('missing y coordinate');
   if(!direction || !~DIRECTIONS.indexOf(direction)) 
     throw new Error('missing or invalid direction (' + DIRECTIONS.join('|') + ')');
 
