984c44c468c39b822fdf09b303758e84b5f26d07
mattes <matthias.kadenbach@gmail.com>
2013-04-11 23:30:13 +0200

    updated test for obstacle collision

--- spec/rover_on_map.spec.js
+++ spec/rover_on_map.spec.js
@@ -2,34 +2,39 @@
 var Map = require('../classes/Map.js');
 var Rover = require('../classes/Rover.js');
 var Obstacle = require('../classes/Obstacle.js');
+var Position = require('../classes/Position.js');
 
 describe('Rover on Map', function(){
 
   describe('moves FORWARD from one edge of the map to the other', function(){
 
     it('from west to east', function(){
-      var rover = new Rover({start: new Position(0, 0, 'east'), map: new Map(3, 3)}).move('ff');
+      var rover = new Rover({start: new Position(0, 0, 'east'), map: new Map(3, 3)});
+      rover.move('ff');
       var position = rover.getPosition();
       expect(position.x).toEqual(-1);
       expect(position.y).toEqual(0);
     });
 
     it('from east to west', function(){
-      var rover = new Rover({start: new Position(0, 0, 'west'), map: new Map(3, 3)}).move('ff');
+      var rover = new Rover({start: new Position(0, 0, 'west'), map: new Map(3, 3)});
+      rover.move('ff');
       var position = rover.getPosition();
       expect(position.x).toEqual(1);
       expect(position.y).toEqual(0);  
     });
 
     it('from north to south', function(){
-      var rover = new Rover({start: new Position(0, 0, 'south'), map: new Map(3, 3)}).move('ff');
+      var rover = new Rover({start: new Position(0, 0, 'south'), map: new Map(3, 3)});
+      rover.move('ff');
       var position = rover.getPosition();
       expect(position.x).toEqual(0);
       expect(position.y).toEqual(1);   
     });
 
     it('from south to north', function(){
-      var rover = new Rover({start: new Position(0, 0, 'north'), map: new Map(3, 3)}).move('ff');
+      var rover = new Rover({start: new Position(0, 0, 'north'), map: new Map(3, 3)});
+      rover.move('ff');
       var position = rover.getPosition();
       expect(position.x).toEqual(0);
       expect(position.y).toEqual(-1);   
@@ -40,28 +45,32 @@ describe('Rover on Map', function(){
   describe('moves BACKWARD from one edge of the map to the other', function(){
 
     it('from east to west', function(){
-      var rover = new Rover({start: new Position(0, 0, 'east'), map: new Map(3, 3)}).move('bb');
+      var rover = new Rover({start: new Position(0, 0, 'east'), map: new Map(3, 3)});
+      rover.move('bb');
       var position = rover.getPosition();
       expect(position.x).toEqual(1);
       expect(position.y).toEqual(0);
     });
 
     it('from west to east', function(){
-      var rover = new Rover({start: new Position(0, 0, 'west'), map: new Map(3, 3)}).move('bb');
+      var rover = new Rover({start: new Position(0, 0, 'west'), map: new Map(3, 3)});
+      rover.move('bb');
       var position = rover.getPosition();
       expect(position.x).toEqual(-1);
       expect(position.y).toEqual(0);  
     });
 
     it('from south to north', function(){
-      var rover = new Rover({start: new Position(0, 0, 'south'), map: new Map(3, 3)}).move('bb');
+      var rover = new Rover({start: new Position(0, 0, 'south'), map: new Map(3, 3)});
+      rover.move('bb');
       var position = rover.getPosition();
       expect(position.x).toEqual(0);
       expect(position.y).toEqual(-1);   
     });
 
     it('from north to south', function(){
-      var rover = new Rover({start: new Position(0, 0, 'north'), map: new Map(3, 3)}).move('bb');
+      var rover = new Rover({start: new Position(0, 0, 'north'), map: new Map(3, 3)});
+      rover.move('bb');
       var position = rover.getPosition();
       expect(position.x).toEqual(0);
       expect(position.y).toEqual(1);   
@@ -70,26 +79,39 @@ describe('Rover on Map', function(){
   });
 
   describe('colides with obstacles', function(){
-    var map = new Map(5, 5);
-    map.addObstacle(new obstacles(1, 1));
-
-    var rover = new Rover();
-    rover.addMap(map);
-
-    it('from west to east', function(){
-      expect(false).toBe(true);    
-    });
+    var map = new Map(3, 3);
+    map.addObstacle(new Obstacle(0, 0));
 
     it('from east to west', function(){
-      expect(false).toBe(true);    
+      var rover = new Rover({start: new Position(1, 0, 'west'), map: map});
+      rover.move('ff');
+      var position = rover.getPosition();
+      expect(position.x).toEqual(1);
+      expect(position.y).toEqual(0);  
+    });
+
+    it('from west to east', function(){
+      var rover = new Rover({start: new Position(-1, 0, 'east'), map: map});
+      rover.move('ff');
+      var position = rover.getPosition();
+      expect(position.x).toEqual(-1);
+      expect(position.y).toEqual(0);     
     });
 
     it('from north to south', function(){
-      expect(false).toBe(true);    
+      var rover = new Rover({start: new Position(0, 1, 'south'), map: map});
+      rover.move('ff');
+      var position = rover.getPosition();
+      expect(position.x).toEqual(0);
+      expect(position.y).toEqual(1);    
     });
 
     it('from south to north', function(){
-      expect(false).toBe(true);    
+      var rover = new Rover({start: new Position(0, -1, 'north'), map: map});
+      rover.move('ff');
+      var position = rover.getPosition();
+      expect(position.x).toEqual(0);
+      expect(position.y).toEqual(-1);     
     });
 
   });
